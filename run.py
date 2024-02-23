30be7d23fd6646d764c98a79b7b9cd5655c61924
Taylor carr <taylormichaelcarr@yahoo.com>
2024-02-23 01:59:21 +0000

    added sales data

--- run.py
+++ run.py
@@ -39,6 +39,18 @@ def validate_data(values):
     except ValueError as e :
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
+    return True
+
+def update_sales_worksheet(data):
+
+    print("Updating sales worksheet\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
